5d3a2d0bee51cfe668e45c4d7fc38634c29b8ce3
lasassessoriaead-a11y <lasassessoriaead@gmail.com>
2026-09-02 13:46:03 -0300

    fix: use ESM .js extension for Mercado Livre shared helper in import.ts

--- api/mercadolivre/import.ts
+++ api/mercadolivre/import.ts
@@ -1,4 +1,4 @@
-import { getMercadoLivreSession, requireSupabaseUser } from '../../server/mercadolivre'
+import { getMercadoLivreSession, requireSupabaseUser } from '../../server/mercadolivre.js'
 
 export default async function handler(req: any, res: any) {
   res.setHeader('Cache-Control', 'no-store')
